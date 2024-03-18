f9f377356d1aed97738eee41f851cd99c38069ff
ibrahim <hammicheibrahimm@gmail.com>
2024-03-18 07:13:25 -0700

    exo 1

---
+++ src/test/java/CalculatriceTest.java
@@ -0,0 +1,38 @@
+import org.example.Calculatrice;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CalculatriceTest {
+    @Mock
+    private Calculatrice calculatrice;
+    @Test
+    public void testAdditionner() {
+        // Définition du comportement de la méthode "additionner"
+        when(calculatrice.additionner(2, 3)).thenReturn(5);
+        //TODO : Appel de la méthode à tester
+        // Act
+        int resultat = calculatrice.additionner(2,3);
+        //TODO : Vérification du résultat
+        assertEquals(5, resultat);
+        //TODO : Vérification que la méthode "additionner" a été appelée
+        // vec les arguments 2 et 3, utiliser la directive « verify »
+        verify(calculatrice).additionner(2, 3);
+        //TODO : Vérification qu'aucune autre méthode n'a été appelée sur
+        //l'objet après l'appel de la méthode "additionner", utiliser la
+        // méthode « verifyNoMoreInteractions »
+        verifyNoMoreInteractions(calculatrice);
+        // TODO : Vérification de l'état de l'objet après l'appel de la
+        //méthode "additionner", penser à utiliser la méthode
+        //« getState() » de la directive « verify » : // exemple :
+        verify(calculatrice).getState();
+
+    }
+}
